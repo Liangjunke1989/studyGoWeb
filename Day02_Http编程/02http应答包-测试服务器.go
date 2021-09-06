96208383fcb4bdce5a169f9db4bca6a8680b0684
梁俊可 <liangjunke198909@outlook.com>
2021-09-06 15:07:05 +0800

    20210906_GoHttp编程_02创建web服务器

---
+++ Day02_Http编程/02http应答包-测试服务器.go
@@ -0,0 +1,20 @@
+package main
+
+import "net/http"
+
+func main() {
+	/*
+		1、http应答包
+			http响应报文格式
+		2、注册回调行数：
+			该回调函数会在服务器被访问时，自动被调用。
+	*/
+	//创建web服务器
+	//01、注册回调行数
+	http.HandleFunc("/LJK3D", LJK_Handler)
+	//02、绑定服务器监听地址
+	http.ListenAndServe("192.168.1.90:5566", nil) //调用自带的handler
+}
+func LJK_Handler(w http.ResponseWriter, r *http.Request) {
+
+}
