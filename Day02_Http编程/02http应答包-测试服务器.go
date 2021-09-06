87522d394fbd672ffd86c03391255b42276d391a
梁俊可 <liangjunke198909@outlook.com>
2021-09-06 15:32:20 +0800

    20210906_GoHttp编程_02创建web服务器

--- Day02_Http编程/02http应答包-测试服务器.go
+++ Day02_Http编程/02http应答包-测试服务器.go
@@ -15,6 +15,10 @@ func main() {
 	//02、绑定服务器监听地址
 	http.ListenAndServe("192.168.1.90:5566", nil) //调用自带的handler
 }
-func LJK_Handler(w http.ResponseWriter, r *http.Request) {
 
+// LJK_Handler 在服务器被访问时，自动被调用。
+func LJK_Handler(w http.ResponseWriter, r *http.Request) {
+	//w: 响应数据（回写给客户端的数据）
+	//r:请求数据（客户端发送来的请求）
+	w.Write([]byte("LJK的第一条goWeb响应数据：Hello World！！！"))
 }
