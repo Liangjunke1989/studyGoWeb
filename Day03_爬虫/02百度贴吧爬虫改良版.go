fa5029ab84d24cbc7e31b7cc95063f47411f5dcc
梁俊可 <2583493258@qq.cpm>
2021-09-08 11:05:57 +0800

    20210908_爬虫开发_01百度贴吧爬虫-改良

--- Day03_爬虫/02百度贴吧爬虫改良版.go
+++ Day03_爬虫/02百度贴吧爬虫改良版.go
@@ -6,7 +6,10 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"sync"
 )
+
+var wg = sync.WaitGroup{}
 
 func main() {
 	//01、指定爬取起始页、终止页
@@ -16,22 +19,24 @@ func main() {
 	fmt.Print("请输入爬取的终止页（>=start）:")
 	fmt.Scan(&end)
 	//02、爬取页面操作方法
-	DataCrawlerWorking(start, end)
+	DataCrawlerWorking2(start, end)
+	//03、提示操作结束
+	fmt.Println("爬取成功了！主go程结束...")
 }
 
-func DataCrawlerWorking(start int, end int) {
+func DataCrawlerWorking2(start int, end int) {
 	fmt.Printf("正在爬取第%d到%d页...\n", start, end)
-	pageChan := make(chan int)
+	//pageChan:=make(chan int)
+
+	sumThread := end - start + 1
+	wg.Add(sumThread)
 	//循环爬取每一页数据
 	for i := start; i <= end; i++ {
-		go CrawlingSinglePage(i, pageChan)
+		go CrawlingSinglePage2(i)
 	}
-	for i := start; i <= end; i++ {
-		n := <-pageChan
-		fmt.Printf("第%d个页面爬取完成\n", n)
-	}
+	wg.Wait()
 }
-func LJKErrFunc(err error, info string) {
+func LJKErrFunc2(err error, info string) {
 	if err != nil {
 		fmt.Println(info, err.Error())
 		//os.Exit(1)       //将当前进程结束，0值代表正常结束。非零值代表非正常结束。
@@ -39,15 +44,15 @@ func LJKErrFunc(err error, info string) {
 	}
 }
 
-// CrawlingSinglePage 爬取单个页面的函数
-func CrawlingSinglePage(i int, ch chan<- int) {
+// CrawlingSinglePage2 爬取单个页面的函数
+func CrawlingSinglePage2(i int) {
 	url := "https://tieba.baidu.com/f?kw=unity&ie=utf-8&pn=" + strconv.Itoa((i-1)*50) // Itoa() 将整型转成字符串
-	result, err := HttpGet(url)
+	result, err := HttpGet2(url)
 	//if err!=nil {
 	//	fmt.Println("httpGet有误！",err.Error())
 	//	continue
 	//}
-	LJKErrFunc(err, "httpGet有误！")
+	LJKErrFunc2(err, "httpGet有误！")
 	//fmt.Println("result=",result) //测试
 
 	//将读取到的整网页数据，保存成一个文件
@@ -56,13 +61,13 @@ func CrawlingSinglePage(i int, ch chan<- int) {
 	fPath := "./src/Day03_爬虫/DirTest/"
 	fPathName := fPath + fName
 	f2, err := os.OpenFile(fPathName, os.O_RDWR|os.O_CREATE, os.ModePerm)
-	LJKErrFunc(err, "打开文件有误！")
+	LJKErrFunc2(err, "打开文件有误！")
 	f2.WriteString(result)
 	f2.Close() // 保存好一个文件，关闭一个文件。
-	ch <- i
+	wg.Done()
 }
 
-func HttpGet(url string) (result string, err error) {
+func HttpGet2(url string) (result string, err error) {
 	resp, err1 := http.Get(url)
 	if err1 != nil {
 		err = err1 //将封装函数内部的错误，传给调用者
